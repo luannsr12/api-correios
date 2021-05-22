ca7ab675220cb22b671cb38c11530c7a8a8e609c
Luan Alves <32730441+luannsr12@users.noreply.github.com>
2021-05-21 23:13:57 -0300

    Atualização com adição para recuperar tipos de envio

--- index.php
+++ index.php
@@ -1,17 +1,26 @@
 <?php
 
-// Para buscar o status do objeto , basta enviar uma requisição
-// para o arquivo " obj.php " dentro de " api " .
-// O arquivo " obj.php " buscara o status do objeto no próprio site dos correios
+ // Para buscar o status do objeto , basta enviar uma requisição
+ // para o arquivo " obj.php " dentro de " api " .
+ // O arquivo " obj.php " buscara o status do objeto no próprio site dos correios
 
-// PHP >= 5.6
-// @author : Luan Alves
-// Jul / 2019
+ // PHP >= 5.6
+ // @author : Luan Alves
+ // Jul / 2019
 
-//códigos de rastreio separados por ";"
-$obj = "OH215715636BR;OM299200663BR";
-$url = "http://localhost/rastreio/api/obj.php?obj={$obj}";
-$rastreio = json_decode(file_get_contents($url));
+ // Api Url
+ $apiUrl = "http://localhost/rastreio/api";
 
-echo '<pre>';
-var_dump($rastreio);
+
+ // Rastreio objeto - Para mais código adicionar ;
+ $obj      = "CODIGO DE RASTREIO";
+ $rastreio = file_get_contents("{$apiUrl}/obj.php?obj={$obj}");  // Json return
+
+ // Nome do envio conforme a sigla do código
+ $json          = json_decode(file_get_contents("{$apiUrl}/siglas_rastreio.json"));
+ $sigla         = substr($obj,0,2);
+ $tipoEncomenda = $json->$sigla->name; // SEDEX
+
+
+
+?>
